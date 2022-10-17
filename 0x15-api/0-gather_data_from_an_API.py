117bebb4f6dcd912459915559677c8deb6ebbe6f
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-10-17 12:48:31 +0100

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A Python script that, using this REST API,
+for a given employee ID, returns information about his/her TODO list progress
+"""
+from argparse import ArgumentParser
+from os import path
+from requests import get
+from sys import argv
+
+
+mockApi = "https://jsonplaceholder.typicode.com/"
+
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser(prog=path.basename(argv[0]))
+    parser.add_argument('ID', type=int, help="employee ID")
+    args = parser.parse_args()
+    user = get('/'.join([mockApi, 'users', str(args.id)])).json()
+    todo = get('/'.join([mockApi, 'todos']), params={'userId': args.id}).json()
+    completed = [task for task in todo if task['completed'] is True]
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user['name'], len(completed), len(todo)))
+    print('\n'.join('\t {}'.format(task['title']) for task in completed))
